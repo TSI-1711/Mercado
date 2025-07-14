08270d74bb95d76402c7ab71b54a0a91ac58fcbb
Isaque Melo <isaquemp77@gmail.com>
2025-07-14 08:26:10 -0400

    Corrigindo "content" para "conteudo" no section da home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('template')
 
-@section('content')
+@section('conteudo')
     <h1>Sistema ERP para Mercado</h1>
     <p>Este sistema foi desenvolvido na disciplina de Construção de Páginas Web IV do curso de Tecnologia em Sistemas para Internet no período do 1o. Semestre de 2025</p>
 @endsection
